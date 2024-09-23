b97e260f6bae583eff75e71755a6b92b75a3d61c
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-09-23 18:36:02 +0200

    Created asp core project

---
+++ fisher-folio/fisher-folio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace fisher_folio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
