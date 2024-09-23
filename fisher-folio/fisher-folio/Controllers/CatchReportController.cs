86f9242aef8d13fc3830d967ad47753a870e54b2
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-09-23 22:07:55 +0200

    Implemented hero section and styled it with Bootstrap, added CatchReportController and initial models.

---
+++ fisher-folio/fisher-folio/Controllers/CatchReportController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using fisher_folio.Models;
+
+namespace fisher_folio.Controllers
+{
+    public class CatchReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            // TEST  Test for object  TEST
+            Catch fishCatch = new Catch
+            {
+                CatchId = 1,
+                UserId = 1, // Antag, at dette er ID for en eksisterende bruger
+                FishSpecies = "Trout",
+                Location = "Lake X",
+                CatchDate = DateTime.Now
+            };
+            //TEST  Test for object  TEST
+            return View(fishCatch);
+        }
+    }
+}
---
+++ fisher-folio/fisher-folio/Models/Catch.cs
@@ -0,0 +1,14 @@
+﻿namespace fisher_folio.Models
+{
+    public class Catch
+    {
+        // Nøgler
+        public int CatchId { get; set; }    // Primary key
+        public int UserId { get; set; }     // Forrent key
+        public User User { get; set; }      // Relation. En user kan have flere catch. 
+
+        public string FishSpecies { get; set; }
+        public string Location { get; set; }
+        public DateTime CatchDate { get; set; }
+    }
+}
