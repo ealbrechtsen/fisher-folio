78fd7b2e2168650bd5c466fd63e7fdea99d5f9e6
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-10-02 22:02:23 +0200

    Started implementing required for models

--- fisher-folio/fisher-folio/Models/Catch.cs
+++ fisher-folio/fisher-folio/Models/Catch.cs
@@ -1,4 +1,6 @@
-﻿namespace fisher_folio.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fisher_folio.Models
 {
     public class Catch
     {
@@ -7,8 +9,14 @@
         public int UserId { get; set; }     // Forrent key
         public User User { get; set; }      // Relation. En user kan have flere catch. 
 
+        [Required]
         public string FishSpecies { get; set; }
+
+        [Required]        
         public string Location { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime CatchDate { get; set; }
     }
 }
