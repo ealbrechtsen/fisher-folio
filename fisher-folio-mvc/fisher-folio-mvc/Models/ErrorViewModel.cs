d39140aea3810eb91843b9b7e53051622d242c2a
Emil Albrechtsen <145019248+ealbrechtsen@users.noreply.github.com>
2024-10-14 22:01:35 +0200

    created two new projects MVC and API. Gonna replace with old MVC due to lack of authentication

---
+++ fisher-folio-mvc/fisher-folio-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace fisher_folio_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
